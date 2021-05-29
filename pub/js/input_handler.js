00f891226bc35030b2dbe01d37650a0dd337a05a
TurtleKitty <joshua@anwu.org>
2021-05-29 14:14:16 -0500

    Got state machine working again by removing all ES6 import/export

--- pub/js/input_handler.js
+++ pub/js/input_handler.js
@@ -1,92 +1,115 @@
 
-var keymap = {
-   start: {
-      "c": "create",
-      "f": "form",
-      "g": "go"
-   },
-   "choose-adventure": {
-      "b": "back"
+var RuinsOf1985InputHandler = (() => {
+
+   const keymap = {
+      start: {
+         "c": "create",
+         "f": "form",
+         "g": "go"
+      },
+      "choose-adventure": {
+         "b": "back"
+      }
+   };
+
+   var fsm = null;
+
+   function wireKeys () {
+      window.addEventListener(
+         'keypress',
+         function(e) {
+            const dispatch = keymap[fsm.state];
+
+            if (dispatch && dispatch[e.key]) {
+               fsm[dispatch[e.key]]();
+            }
+         }
+      );
    }
-};
 
-function showScreen (name) {
-   hideScreens();
-   document.getElementById(name + "-screen").style.display = "inline-block";
-}
-
-function showMenu (name) {
-   hideMenus();
-   document.getElementById(name + "-menu").style.display = "inline-block";
-}
-
-function hideAll (name) {
-   Array.prototype.slice.call(
-      document.getElementsByClassName(name)
-   ).forEach( function (screen) {
-      screen.style.display = "none";
-   });
-}
-
-function hideScreens () {
-   hideAll("screen");
-}
-
-function hideMenus () {
-   hideAll("menu");
-}
-
-function startState () {
-   // switch screen and menu
-   // wire up keys
-   console.log("Start");
-   showScreen("start");
-   showMenu("start");
-}
-
-function createAdventurerState () {
-   console.log("Create Adventurer");
-}
-
-function saveAdventurerState () {
-
-}
-
-function formPartyState () {
-   console.log("Form a Party");
-}
-
-function savePartyState () {
-
-}
-
-function chooseAdventureState () {
-   console.log("Choose your own adventure!");
-   hideScreens();
-   showScreen("game-main");
-}
-
-
-// Not every button click needs to change state.
-// May be no need for save-adventurer et al.
-
-var fsm = new StateMachine({
-   init: 'start',
-   transitions: [
-      { name: "create",  from: "start",  to: "create-adventurer" },
-      { name: "form",    from: "start",  to: "form-party"        },
-      { name: "go",      from: "start",  to: "choose-adventure"  },
-      { name: "back",    from: [ "create-adventurer", "form-party", "choose-adventure" ],  to: "start" },
-      { name: "save",    from: "create-adventurer",  to: "save-adventurer" },
-      { name: "save",    from: "form-party",         to: "save-party" },
-   ],
-   methods: {
-      onStart: startState,
-      onCreateAdventurer: createAdventurerState,
-      onFormParty: formPartyState,
-      onChooseAdventure: chooseAdventureState
+   function showScreen (name) {
+      hideScreens();
+      document.getElementById(name + "-screen").style.display = "inline-block";
    }
-});
 
-export { keymap, fsm };
+   function showMenu (name) {
+      hideMenus();
+      document.getElementById(name + "-menu").style.display = "inline-block";
+   }
 
+   function hideAll (name) {
+      Array.prototype.slice.call(
+         document.getElementsByClassName(name)
+      ).forEach( function (screen) {
+         screen.style.display = "none";
+      });
+   }
+
+   function hideScreens () {
+      hideAll("screen");
+   }
+
+   function hideMenus () {
+      hideAll("menu");
+   }
+
+   function startState () {
+      // switch screen and menu
+      // wire up keys
+      console.log("Start");
+      showScreen("start");
+      showMenu("start");
+   }
+
+   function createAdventurerState () {
+      console.log("Create Adventurer");
+   }
+
+   function saveAdventurerState () {
+
+   }
+
+   function formPartyState () {
+      console.log("Form a Party");
+   }
+
+   function savePartyState () {
+
+   }
+
+   function chooseAdventureState () {
+      console.log("Choose your own adventure!");
+      hideScreens();
+      showScreen("game-main");
+   }
+
+   function startFSM() {
+      // Not every button click needs to change state.
+      // May be no need for save-adventurer et al.
+
+      fsm = new StateMachine({
+         init: 'start',
+         transitions: [
+            { name: "create",  from: "start",  to: "create-adventurer" },
+            { name: "form",    from: "start",  to: "form-party"        },
+            { name: "go",      from: "start",  to: "choose-adventure"  },
+            { name: "back",    from: [ "create-adventurer", "form-party", "choose-adventure" ],  to: "start" },
+            { name: "save",    from: "create-adventurer",  to: "save-adventurer" },
+            { name: "save",    from: "form-party",         to: "save-party" },
+         ],
+         methods: {
+            onStart: startState,
+            onCreateAdventurer: createAdventurerState,
+            onFormParty: formPartyState,
+            onChooseAdventure: chooseAdventureState
+         }
+      });
+
+      wireKeys();
+   }
+
+   return {
+      startFSM: startFSM
+   };
+
+})();
--- pub/js/ruins.js
+++ pub/js/ruins.js
@@ -1,25 +1,10 @@
 
-import * as Input from "../js/input_handler.js";
+var RuinsOf1985 = (() => {
+   var Input = window.RuinsOf1985InputHandler;
 
-console.log(Input.keymap);
+   function run () {
+      Input.startFSM();
+   }
 
-function wireKeys () {
-   window.addEventListener(
-      'keypress',
-      function(e) {
-         var dispatch = Input.keymap[Input.fsm.state];
-
-         if (dispatch && dispatch[e.key]) {
-            Input.fsm[dispatch[e.key]]();
-         }
-      }
-   );
-}
-
-function run () {
-   Input.fsm.start();
-}
-
-wireKeys(); // why doesn't this work inside of run()?
-
-export { run };
+   return { run: run };
+})();
